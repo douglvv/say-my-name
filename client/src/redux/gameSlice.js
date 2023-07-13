d275afc48b630fe93b7febc1267930c864c4ea8f
Douglas Vidal <vidaldl01@gmail.com>
2023-07-13 13:18:13 -0300

    redux game

---
+++ client/src/redux/gameSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    id: null,
+    players: [],
+    quote: {},
+    quoteHistory: [],
+    quotesLeft: null,
+}
+
+export const gameSlice = createSlice({
+    name: "game",
+    initialState: initialState,
+    reducers: {
+        updateGameState: (state, action) => {
+            state = action.payload.game; 
+        },
+        finishGame: (state) => {
+            state = initialState;
+        }
+    }
+});
+
+export const { updateGameState, finishGame } = gameSlice.actions;
+export default gameSlice.reducer;
