688920952c58c4be44e96f12df1726be4db79a88
Douglas Vidal <vidaldl01@gmail.com>
2023-07-26 16:39:45 -0300

    play again / quit buttons

---
+++ client/src/components/GameFinishedButtons/GameFinishedButtons.jsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { Row, Col, Button } from "react-bootstrap"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom";
+import { SocketContext } from "../../contexts/SocketContext";
+
+
+const GameFinishedButtons = () => {
+    const gameState = useSelector((state) => state.game.game);
+    const playerState = useSelector((state) => state.game.player);
+    const navigate = useNavigate();
+    const socket = useContext(SocketContext);
+
+    const playAgain = () => {
+        socket.emit("playAgain", { gameId: gameState.id })
+    }
+
+    const quit = () => {
+        socket.emit("quit", { gameId: gameState.id })
+
+        navigate('/');
+    }
+
+    return (
+        <Row className="my-3 mx-1">
+            <Col>
+                <Button
+                    className="w-100 py-3 px-0"
+                    variant="primary"
+                    disabled={!playerState.isTurn}
+                    onClick={playAgain}
+                >
+                    Play Again
+                </Button>
+            </Col>
+            <Col>
+                <Button
+                    className="w-100 py-3 px-0"
+                    variant="secondary"
+                    disabled={false}
+                    onClick={quit}
+                >
+                    Quit
+                </Button>
+            </Col>
+        </Row>
+    )
+}
+
+export default GameFinishedButtons;
--- client/src/screens/GameFinishedScreen.jsx
+++ client/src/screens/GameFinishedScreen.jsx
@@ -1,12 +1,13 @@
 import { Container } from "react-bootstrap";
 import GameFinished from "../components/GameFinished/GameFinished";
-
+import GameFinishedButtons from "../components/GameFinishedButtons/GameFinishedButtons";
 
 const GameFinishedScreen = () => {
     return (
         <>
             <Container className="vh-100 text-center">
                 <GameFinished />
+                <GameFinishedButtons />
             </Container>
         </>
     );
